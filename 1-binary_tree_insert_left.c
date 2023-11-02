b5f9e9a8f07c487fef351a9e4b7da48838364ccd
Arnold Henry <devharnold@gmail.com>
2023-11-02 11:36:56 +0300

    make-changes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,22 +2,6 @@
 #include <binary_trees.h>
 #include <stdio.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
 
 /**
  * int value : the representation of node value
@@ -26,8 +10,6 @@ typedef struct binary_tree_s binary_tree_t;
  * @parent: The parent node
  * 
 */
-
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
     if (parent == NULL){
         return NULL;
